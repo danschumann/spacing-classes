6179f2131340875b1840942da55c1e393e668e72
danschumann <dan.schumann@gmail.com>
2014-06-02 14:30:34 -0500

    Added hooks for require() and compile

--- index.js
+++ index.js
@@ -1,59 +1,8 @@
-var
-  // We build stylus code and let stylus do the extra formatting for us
-  join        = require('path').join,
-  fs          = require('fs'),
-  stylus      = require('stylus'),
-  styl        = '/*! https://github.com/danschumann/spacing-classes/ */',
-  indent      = '',
-  _           = require('underscore'),
-  directions  = [ 'top', 'left', 'right', 'bottom', '' ],
-  sizes       = [ 3, 7, 15, 22, 30 ];
-
-function make (type, cssType, inverse) {
-  _.each({
-    xs: null,
-    sm: 768,
-    md: 992,
-    lg: 1200,
-  }, function(minWidth, screenSize) {
-
-    if (minWidth) {
-      styl += '\n@media (min-width: ' + minWidth + 'px)';
-      indent = '  ';
-    } else {
-      indent = '';
-    };
-
-
-    _.each(sizes, function(pixels) {
-      _.each(directions, function(dir) {
-        if (dir)
-          dir = '-' + dir;
-        styl += '\n' + indent + '.' + type + '-' + screenSize + dir + '-' + pixels + (inverse ? '-inv' : '');
-        styl += '\n  ' + indent + cssType + dir + ': ' + (inverse ? '-' : '') + pixels + 'px';
-      });
-
-    });
-
-  })
+// stylus('path').import(require('spacing-classes'));
+// we only export the path to import
+module.exports = function(styl) {
+  styl.include(__dirname);
 }
 
-make('pad', 'padding')
-make('marg', 'margin')
-make('marg', 'margin', true)
-
-console.log(styl);
-
-stylus.render(styl, function(err, css) {
-  console.log(err, css);
-
-  fs.writeFile(join(__dirname, 'spacing-classes.css'), css);
-  
-})
-
-stylus.render(styl, {compress: true}, function(err, css) {
-  console.log(err, css);
-
-  fs.writeFile(join(__dirname, 'spacing-classes.min.css'), css);
-  
-})
+// expose compile method in case they don't want defaults
+module.exports.compile = require('./compile')
